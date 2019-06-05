794aca66d44748031d93ef642a83e4ac157d9cc0
dordonez <dordonez@ute.edu.ec>
2019-06-05 14:20:51 -0500

    Update

---
+++ basico-uno/src/main/java/ute/apdist/basicouno/BasicoUnoApplication.java
@@ -0,0 +1,36 @@
+package ute.apdist.basicouno;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BasicoUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicoUnoApplication.class, args);
+	}
+
+	/*
+	@Bean
+	public Date initDate() {
+		Date d = new Date();
+		System.out.println(d);
+		return d;
+	}
+	
+	@Bean
+	public List<String> initList() {
+		List<String> l = new LinkedList<>();
+		l.add("Diego");
+		l.add("Ana");
+		System.out.println(l);
+		return l;
+	}	
+	*/
+	
+}
