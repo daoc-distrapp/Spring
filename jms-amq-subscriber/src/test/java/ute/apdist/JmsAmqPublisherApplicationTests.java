990b42fe319ce1f6694d0766e7501ab8667a000f
diego <diego.ordonez@gmail.com>
2019-06-17 08:40:44 -0500

    Initial AMQ Subscriber

---
+++ jms-amq-subscriber/src/test/java/ute/apdist/JmsAmqPublisherApplicationTests.java
@@ -0,0 +1,16 @@
+package ute.apdist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmsAmqPublisherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
