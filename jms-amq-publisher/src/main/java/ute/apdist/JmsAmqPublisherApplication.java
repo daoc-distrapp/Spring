0aed8983e64c7f5a7d013e2afef78f34f55e1759
diego <diego.ordonez@gmail.com>
2019-06-17 08:09:03 -0500

    Initial JMS Publisher

---
+++ jms-amq-publisher/src/main/java/ute/apdist/JmsAmqPublisherApplication.java
@@ -0,0 +1,13 @@
+package ute.apdist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsAmqPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsAmqPublisherApplication.class, args);
+	}
+
+}
