4fa89af7797cd19724c33118e262a9238c26cc3e
dordonez <dordonez@ute.edu.ec>
2019-06-27 09:07:27 -0500

    Update
    Web static

---
+++ web-static/src/main/java/com/example/webstatic/WebStaticApplication.java
@@ -0,0 +1,13 @@
+package com.example.webstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStaticApplication.class, args);
+	}
+
+}
