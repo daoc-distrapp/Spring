286e4342be7df8ca9be7ae499e7bb1b1cf169e98
dordonez <dordonez@ute.edu.ec>
2019-06-28 07:46:40 -0500

    Update
    Web security simple

---
+++ web-security/src/main/java/com/example/websecurity/WebSecurityApplication.java
@@ -0,0 +1,24 @@
+package com.example.websecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecurityApplication {
+
+	/**
+	 * Si se incluye spring-boot-starter-security en el pom, la aplicación se asegura automáticamente
+	 * Al intentar ingresar el sistema redirige a: /login
+	 * El usuario por defecto es: user
+	 * El password por defecto se genera cada vez que se arranca la aplicación y se ve en la consola. Ej:
+	 *   Using generated security password: dea238a0-559e-4e98-92ac-b7ece3c73df7
+	 * Si se ingresan las credenciales correctamente, se redirecciona a la página inicial: /index.html
+	 * Si se ingresan las credenciales erróneas, se redirecciona a: /login?error
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecurityApplication.class, args);
+	}
+
+}
