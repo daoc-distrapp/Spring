709e4100b24a8b1ff7e678341c043f369331e2a7
daoc <diego.ordonez@gmail.com>
2019-06-29 17:07:59 -0500

    Update

--- web-security-users/src/main/java/com/example/websecurity/WebSecurityConfig.java
+++ web-security-users/src/main/java/com/example/websecurity/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,7 +12,7 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	    
+	
 	@Bean
 	public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
 		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
@@ -22,6 +23,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/user.html").hasRole("USER")
+			.antMatchers("/admin.html").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and().formLogin();
 	}
 	
 }
