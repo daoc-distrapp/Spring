ce5a9557822492cf4769f8c0694cbceace44145c
daoc <diego.ordonez@gmail.com>
2019-06-30 13:30:25 -0500

    Update

--- web-security-users/src/main/java/com/example/websecurity/WebSecurityApplication.java
+++ web-security-users/src/main/java/com/example/websecurity/WebSecurityApplication.java
@@ -1,35 +1,24 @@
 package com.example.websecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
-public class WebSecurityApplication implements ApplicationRunner {
+public class WebSecurityApplication {
 
-	@Autowired
-	JdbcUserDetailsManager jdbcUserDetailsManager;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
+	@Bean
+	public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+		jdbcUserDetailsManager.setDataSource(dataSource);
+		return jdbcUserDetailsManager;
+	}	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WebSecurityApplication.class, args);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		if(!jdbcUserDetailsManager.userExists("user2")) {
-			jdbcUserDetailsManager.createUser(User.withUsername("user2").password(passwordEncoder.encode("user2")).roles("USER").build());
-		}
-		if(!jdbcUserDetailsManager.userExists("admin2")) {
-			jdbcUserDetailsManager.createUser(User.withUsername("admin2").password(passwordEncoder.encode("admin2")).roles("ADMIN").build());
-		}
 	}
 
 }
