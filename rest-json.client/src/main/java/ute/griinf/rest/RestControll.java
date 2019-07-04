7089752e4b47056ec5d3aae5775c164fada5cdea
dordonez <dordonez@ute.edu.ec>
2019-07-04 15:35:31 -0500

    Update

---
+++ rest-json.client/src/main/java/ute/griinf/rest/RestControll.java
@@ -0,0 +1,36 @@
+package ute.griinf.rest;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControll {
+
+	@RequestMapping("/hola")
+	public String getHola() {
+		return "Hola REST !!!";
+	}
+	
+	@RequestMapping("/holaJson")
+	public UnDato getJson() {
+		return new UnDato(1, "Ana", 10.53);
+	}	
+
+	
+	@RequestMapping("/holaJsonS")
+	public UnDato[] getJsonS() {
+		return new UnDato[]{new UnDato(1, "Ana", 10.53),  new UnDato(2, "Pedro", 5.432)};
+	}		
+	
+	@RequestMapping("/holaJsonSS")
+	public List<UnDato> getJsonSS() {
+		List<UnDato> lista = new LinkedList<>();
+		lista.add(new UnDato(1, "Ana", 10.53));
+		lista.add(new UnDato(2, "Pedro", 5.432));
+		return lista;
+	}		
+	
+}
