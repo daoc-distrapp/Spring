35b2770665912f09d97eaa078bb6c8e2079ae1bd
unknown <dordonez@UIOFCIDTC050.ute.edu.ec>
2019-07-04 20:32:32 +0100

    Update rest

--- rest-json/src/main/java/ute/griinf/rest/RestJsonApplication.java
+++ rest-json/src/main/java/ute/griinf/rest/RestJsonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestXmlJsonApplication {
+public class RestJsonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestXmlJsonApplication.class, args);
+		SpringApplication.run(RestJsonApplication.class, args);
 	}
 
 }
---
+++ rest-json/src/main/java/ute/griinf/rest/UnDato.java
@@ -0,0 +1,15 @@
+package ute.griinf.rest;
+
+public class UnDato {
+	public int numero;
+	public String nombre;
+	public double valor;
+	
+	public UnDato() {}
+	
+	public UnDato(int numero, String nombre, double valor) {
+		this.numero = numero;
+		this.nombre = nombre;
+		this.valor = valor;
+	}
+}
